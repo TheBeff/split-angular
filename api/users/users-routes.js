fb244557d581dbce03c0accc57dfbac695b7b515
TheBeff <thebeff@gmail.com>
2016-10-02 15:13:22 -0400

    added get for users

--- api/users/users-routes.js
+++ api/users/users-routes.js
@@ -2,12 +2,20 @@ const router = require('express').Router();
 const Users = require('../../db/index.js').models.Users;
 
 router.put('/:id', function(req, res, next){
-	Users.findById(req.params.id, {include: Groups})
+	Users.findById(req.params.id})
 	  .then(function(user){
 	  	user.update({password: req.body.password});
 	  })
 	  .then(function(user){
 	  	res.sendStatus(200);
+	  })
+	  .catch(next);
+});
+
+router.get('/:id', function(req, res, next){
+	Users.find({where: {id: req.params.id}}, include: ['Groups'])
+	  .then(function(user){
+	  	res.send(user);
 	  })
 	  .catch(next);
 });
