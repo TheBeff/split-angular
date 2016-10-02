a4ef3bd7f202d6ffede447540ca54ed11f0d9169
TheBeff <thebeff@gmail.com>
2016-10-02 10:41:23 -0400

    added index route

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
-app.use('/browser', express.static(path.join(__dirname, "browser")));
 app.use(express.static(path.join(__dirname, "node_modules")));
+app.use('/browser', express.static(path.join(__dirname, "browser")));
+app.use('/public', express.static(path.join(__dirname, "public")));
+
+app.get('/', function(req, res, next){
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 module.exports = app;
