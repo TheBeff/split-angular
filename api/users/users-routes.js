5d7a1419097fc4cbe5ac4a489abfe694d2bd2b78
TheBeff <thebeff@gmail.com>
2016-10-02 12:19:30 -0400

    built users route
    but its broken

--- api/users/users-routes.js
+++ api/users/users-routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
+const Users = require('../../db/index.js').models.Users;
+
+router.put('/:id', function(req, res, next){
+	console.log(req);
+	Users.findById(req.params.id)
+	  .then(function(user){
+	  	user.update({password: req.body.password});
+	  })
+	  .then(function(user){
+	  	console.log(user.username + "'s password has been updated.");
+	  })
+	  .catch(next);
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, "node_modules")));
 app.use('/browser', express.static(path.join(__dirname, "browser")));
 app.use('/public', express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res, next){
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
